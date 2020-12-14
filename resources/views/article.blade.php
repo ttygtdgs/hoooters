ae13eef28907ac6f854bec913bc3412e46894496
Futa Nakano <zidane10.fn@gmail.com>
2020-12-14 22:58:07 +0900

    futa 12/14
    div分け

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,1 +1,30 @@
-ccccccc
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>投稿ページ</title>
+</head>
+<body>
+  <div class="container">
+  <!-- タイトル -->
+    <div class="title-wrapper">
+      <div class="cname-wrapper">会社名</div>
+      <div class="service-wrapper"></div>
+      <div class="gyo-wrapper"></div>
+    </div>
+  <!-- 内容 -->
+    <div class="content-wrapper">
+      <div class="image-wrapper"></div>
+      <div class="comment-wrapper"></div>
+    </div>
+
+  <!-- 送信ボタン -->
+    <div class="btn-wrapper"></div>
+  <!-- ヘルプボタン -->
+    <div class="help-wrapper">
+
+    </div>
+  </div>
+</body>
+</html>
