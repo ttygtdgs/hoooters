b211892452f12ac1b60f0f2b0068bdae065228f7
oreo2990 <knnt5323@gmail.com>
2020-12-21 11:19:36 +0900

    コメント、ライク機能完成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -21,14 +21,20 @@ class ArticleController extends Controller
         $texts = Text::join('users','texts.id','=','users.id')->select('texts.created_at as textscreated_at','texts.txt','users.name','users.icon')->orderBy('textscreated_at', 'asc')->get();
         
         $usersicon=Auth::user()->icon;
-        // 対応するaidのデータのみとってくる
-        // $textsnums =  Text::where('aid',$aid)->get();
+        // 対応するaidのデータのみとってくる。ここの「1」を変更！
+        $textsnums =  Text::where('aid',1)->get();
+        $likesnums =  Like::where('aid',1)->get();
+
+        // 対応するlikeの状態データをとってくる。ここの「1」を変更！
+        $likescon =  Like::where('id',Auth::user()->id)->where('aid',1)->get();
 
         // 「article.blade.php」に遷移 & データを渡す
         return view('article',[
             'texts' => $texts,
             'usersicon' => $usersicon,
-            // 'textsnums' => $textsnums,
+            'textsnums' => $textsnums,
+            'likesnums' => $likesnums,
+            'likescon' => $likescon,
              ]);
     }
 
