18f5686aaea214a99375768828e0bc33c7480622
Futa Nakano <zidane10.fn@gmail.com>
2020-12-22 13:01:32 +0900

    画像見れない

---
+++ public/js/postedit.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+  const gid = document.getElementById('gid').value;
+  console.log(gid);
+  switch(gid){
+    case 1:
+      document.getElementById('new').classList.add('act');
+      break;
+    case 2:
+      document.getElementById('webservice').classList.add('act');
+      break;
+    case 3:
+      document.getElementById('production').classList.add('act');
+      break;
+    case 4:
+      document.getElementById('marketing').classList.add('act');
+      break;
+    case 5:
+      document.getElementById('other').classList.add('act');
+      break;
+    case 6:
+      document.getElementById('sier').classList.add('act');
+      break;
+  }
+}
