4a6914f848cbe40ecb2a7edbe76c5cbfc01d8634
Futa Nakano <zidane10.fn@gmail.com>
2020-12-22 13:07:56 +0900

    canvas

--- public/js/postedit.js
+++ public/js/postedit.js
@@ -2,22 +2,22 @@ window.onload = function(){
   const gid = document.getElementById('gid').value;
   console.log(gid);
   switch(gid){
-    case 1:
+    case '1':
       document.getElementById('new').classList.add('act');
       break;
-    case 2:
+    case '2':
       document.getElementById('webservice').classList.add('act');
       break;
-    case 3:
+    case '3':
       document.getElementById('production').classList.add('act');
       break;
-    case 4:
+    case '4':
       document.getElementById('marketing').classList.add('act');
       break;
-    case 5:
+    case '5':
       document.getElementById('other').classList.add('act');
       break;
-    case 6:
+    case '6':
       document.getElementById('sier').classList.add('act');
       break;
   }
